07be47feabf7a1e3d8b84387ff11a279a8e83982
Tentrillicom <69224322+Tentrillicom@users.noreply.github.com>
2021-10-16 15:32:06 -0600

    Update Instant_Music.py

--- Instant_Music.py
+++ Instant_Music.py
@@ -1,7 +1,10 @@
-import os, sys, pip
+import os, sys, pip, getpass
 from pytube import YouTube, Playlist
 print("Instant Music by Tentri#5008")
-carp = "/storage/emulated/0/Dowload"
+if os.name == "posix":
+    carp = "/storage/emulated/0/Dowload"
+else:
+    carp = "C:\\Users\\"+getpass.getuser()+"\\Music"
 
 def desc(yt):
     print(yt.title)
@@ -18,13 +21,13 @@ def get(link):
             pl = Playlist(link)
             for c in pl.video_urls: desc(YouTube(c))
         except:
-            print("Error\n Puede ser causado por una actualización pendiente, desea verificar actualizaciones? (S/N)")
+            print("Error\n It may be caused by a pending update, do you want to check for updates? (Y/N)")
             opc = input()
-            if opc=="s" or opc=="S":
+            if opc=="y" or opc=="Y":
                 if hasattr(pip, 'main'): pip.main(['install', 'pytube', '-U'])
                 else: pip._internal.main(['install', 'pytube', '-U'])
             else: exit()
-            print("Listo, porfavor reinicie la aplicación")
+            print("Done, please restart the application")
             input()
             exit()
 print("Link: ", end="")
