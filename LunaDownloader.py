945ad2fcd249255afae2317759ca4dfc3ee73d79
Tentrillicom <69224322+Tentrillicom@users.noreply.github.com>
2021-10-16 18:21:09 -0600

    Update LunaDownloader.py

--- LunaDownloader.py
+++ LunaDownloader.py
@@ -3,7 +3,5 @@ win = Tk()
 win.title("LunaDownloader")
 win.geometry("360x800")
 win.resizable(False, False)
-text = win.clipboard_get()
-lbl = Label(win, text=text).grid()
+tb = Entry(win).grid()
 win.mainloop()
-
